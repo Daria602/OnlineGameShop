03302b46e2ff586f50b26e722debaa410e470e11
Daria602 <izotovadaria0602@gmail.com>
2023-01-16 16:28:44 +0200

    Added npm install script

---
+++ npminstall.sh
@@ -0,0 +1,11 @@
+npm install @ngneat/falso
+npm install bcryptjs
+npm install express
+npm install express-graphql
+npm install graphql
+npm install jsonwebtoken
+npm install node-fetch
+npm install sequelize
+npm install sqlite3
+npm install nodemon -D
+npm install sequelize-cli -D
