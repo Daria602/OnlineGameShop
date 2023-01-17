52b650956c91835afbf0d9cb7ea051c91dd4164c
Gîrbea Monica <girbeamonica@gmail.com>
2023-01-17 15:14:10 +0200

    Minor changes

--- npminstall.sh
+++ npminstall.sh
@@ -1,8 +1,6 @@
 npm install @ngneat/falso
 npm install bcryptjs
 npm install express
-npm install express-graphql
-npm install graphql
 npm install jsonwebtoken
 npm install node-fetch
 npm install sequelize
